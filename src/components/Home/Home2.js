26559827c75fa1af80610bbf8255f70f53a7a994
nachogl1 <i.gonzalezl@hotmail.com>
2024-10-19 21:53:13 +0100

    deploy: lets go

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -16,34 +16,18 @@ function Home2() {
               An <span className="purple"> summary </span> about myself
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              Positive minded and always full of energy, I am deeply interested
+              in improving in general with a strong focus on productivity.
               <br />
-              <br />I am fluent in classics like
-              <i>
-                <b className="purple"> C++, Javascript and Go. </b>
-              </i>
+              After my degree I started to gain some interest in programming and
+              decided to dedicate myself to it. After finding my first job, I
+              started to work on my own little projects. Now I am also
+              interested in content creation and keep improving in any
+              discipline related to software. I do use TDD while programming and
+              try to advocate for a short feedback loop seeking efficiency.
               <br />
-              <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to <b className="purple">Blockchain.</b>
-              </i>
-              <br />
-              <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
-              <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i>
+              Other interests cover playing and coaching padel, and BJJ
+              (Brazilian Jiujitsu) among others.
             </p>
           </Col>
           <Col md={4} className="myAvtar">
