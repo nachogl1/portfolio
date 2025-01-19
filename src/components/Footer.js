5996d876170940c4dcf4a3fb88d515486f57ce5e
nachogl1 <i.gonzalezl@hotmail.com>
2025-01-19 14:43:53 +0000

    Fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,13 +14,13 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Idea by soumyajit4419</h3>
+          {/* <h3>Ignacio Gonzalez</h3> */}
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} SB</h3>
         </Col>
         <Col md="4" className="footer-body">
-          <ul className="footer-icons">
+          {/* <ul className="footer-icons">
             <li className="social-icons">
               <a
                 href="https://github.com/soumyajit4419"
@@ -61,7 +61,7 @@ function Footer() {
                 <AiFillInstagram />
               </a>
             </li>
-          </ul>
+          </ul> */}
         </Col>
       </Row>
     </Container>
